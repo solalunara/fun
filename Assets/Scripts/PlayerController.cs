b0ffce06e1c75e0034abaf7adccbe8264e62979d
QuantumSkull <1pneumonoultramicroscopic1@gmail.com>
2021-07-12 15:15:39 -0700

    Made the guy able to actually move
    Bottom text

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,40 +4,77 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    CharacterController controller;
+    Rigidbody2D rPlayerBody;
+    BoxCollider2D bPlayerCollider;
+    //CharacterController controller;
 
-    public float speed = 5.0f;
-    public float drag = 20.0f;
+    public int speed = 50;
+    //public float drag = 20.0f;
 
-    private Vector2 _moveDirection = Vector2.zero;
+    //private Vector2 _moveDirection = Vector2.zero;
 
     // Start is called before the first frame update
     void Start()
     {
-        controller = GetComponent<CharacterController>();
+        //controller = GetComponent<CharacterController>();
+        rPlayerBody = GetComponent<Rigidbody2D>();
+        bPlayerCollider = GetComponent<BoxCollider2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
+        //Checks which arrow keys are pressed
+        bool leftPressed = Input.GetKey(KeyCode.LeftArrow);
+        bool rightPressed = Input.GetKey(KeyCode.RightArrow);
+
+        
+        Vector2 netHorizontalForce = NetForceHorizontal(leftPressed, rightPressed);
+
+        Debug.Log(netHorizontalForce);
+
+        rPlayerBody.AddForce(netHorizontalForce);
+
+
+
+
+        /*
         // movedirection lmao
         _moveDirection = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
         _moveDirection *= speed;
 
-        /**
+        
         // jump
         if (Input.GetButton("Jump"))
         {
             _moveDirection.y = jumpSpeed;
         }
-        **/
+        
 
         // apply gravity
         _moveDirection.y -= drag * Time.deltaTime * Time.deltaTime;
 
         // move the character
         controller.Move(_moveDirection);
+       */
 
+    }
+
+    private Vector2 NetForceHorizontal(bool leftPressed, bool rightPressed)
+    {
+        if(leftPressed && rightPressed) return Vector2.zero;
+
+        Vector2 netForceApplied = new Vector2();
+
+        if(leftPressed) 
+        {
+            netForceApplied += Vector2.left*speed*Time.deltaTime;
+        }
+        if(rightPressed)
+        {
+            netForceApplied += Vector2.right*speed*Time.deltaTime;
+        }
+        return netForceApplied;
     }
     
 }
